a881f310e16104c96d79e3b852ae57305eaab59b
codeanddesigngroup <codeanddesigngroup10@gmail.com>
2026-06-12 00:12:44 +0500

    changes

--- src/components/ui/RichTextEditor.tsx
+++ src/components/ui/RichTextEditor.tsx
@@ -2,9 +2,9 @@
 
 import React from 'react';
 import dynamic from 'next/dynamic';
-import 'react-quill/dist/quill.snow.css';
+import 'react-quill-new/dist/quill.snow.css';
 
-const ReactQuill = dynamic(() => import('react-quill'), {
+const ReactQuill = dynamic(() => import('react-quill-new'), {
   ssr: false,
   loading: () => <div className="h-40 w-full bg-gray-50 animate-pulse rounded-xl" />,
 });
